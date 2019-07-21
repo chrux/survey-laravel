55c82859281c58dd5288e8b0dbe9b573e21bac9b
Christian Torres <chtorrez@gmail.com>
2019-07-20 20:48:36 -0600

    Adding BrowserSync and removing generated files.

--- webpack.mix.js
+++ webpack.mix.js
@@ -11,5 +11,18 @@ const mix = require('laravel-mix');
  |
  */
 
-mix.js('resources/js/app.js', 'public/js')
-    .sass('resources/sass/app.scss', 'public/css');
+mix
+    .js('resources/js/app.js', 'public/js')
+    .sass('resources/sass/app.scss', 'public/css')
+    .browserSync({
+        open: 'external',
+        host: 'localhost',
+        proxy: '127.0.0.1:8000',
+        files: [
+            'resources/views/**/*.php',
+            'app/**/*.php',
+            'routes/**/*.php',
+            'public/js/*.js',
+            'public/css/*.css'
+        ]
+    });
