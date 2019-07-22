d8b845128a40a872a4595713b647f605438422ec
Christian Torres <chtorrez@gmail.com>
2019-07-21 22:57:51 -0600

    Improving navigation and style for mobils

--- resources/views/nav.blade.php
+++ resources/views/nav.blade.php
@@ -1,10 +1,12 @@
 @if (Route::has('login'))
 <div class="{{ $class }} links">
-  @auth
-  <a href="{{ url('/home') }}">Home</a>
+  @if (request()->route()->getName() == 'login')
+  <a href="{{ route('home') }}">Home</a>
   @else
   <a href="{{ route('questions.index') }}">Questions</a>
-        
+  @endif
+  @auth
+  @else
   @if (Route::has('register'))
   <a href="{{ route('register') }}">Register</a>
   @endif
