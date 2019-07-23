b8509eff384a51b3c089f543608beb5136aff490
Christian Torres <chtorrez@gmail.com>
2019-07-22 23:36:19 -0600

    Starting to handle events.

--- resources/js/components/FormOptionItemComponent.vue
+++ resources/js/components/FormOptionItemComponent.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="form-group row mt-3">
     <div class="col">
-      <input type="text" class="form-control" name="question[options][][title]" :placeholder="$t('Option ') + number">
+      <input type="text" class="form-control" name="question[options][][title]" :placeholder="$t('Option ') + `${number}`">
     </div>
     <div class="col-auto" v-if="isRemovable">
       <button class="btn btn-danger"><i class="icon ion-md-trash"></i></button>
--- resources/js/components/FormQuestionComponent.vue
+++ resources/js/components/FormQuestionComponent.vue
@@ -2,11 +2,11 @@
   <div class="row justify-content-center">
     <div class="col-md-8">
       <h1 class="text-center">{{ $t('New Question') }}</h1>
-      <form action="" method="post">
+      <form action="" method="post" v-on:submit.prevent="newQuestion()">
         <div class="container-fluid">
           <div class="form-group row mt-3">
             <div class="col">
-              <input type="text" class="form-control" name="question[title]" :placeholder="$t('Question')">
+              <input type="text" class="form-control" name="question[title]" :placeholder="$t('Question')" v-model="questionTitle">
             </div>
           </div>
           <div class="form-group row mt-3">
@@ -28,8 +28,8 @@
             </div>
           </div>
           <div class="options">
-            <form-option-item number="1"></form-option-item>
-            <form-option-item number="2"></form-option-item>
+            <form-option-item :number="1"></form-option-item>
+            <form-option-item :number="2"></form-option-item>
           </div>
           <div class="row mt-3">
             <div class="col">
@@ -41,7 +41,7 @@
           <hr />
           <div class="row mt-3">
             <div class="col-md-4 ml-auto">
-              <button class="btn btn-primary btn-lg btn-block" type="submit">{{ $t('Create') }}</button>
+              <button class="btn btn-primary btn-lg btn-block" type="submit" v-bind:disabled="!isValid">{{ $t('Create') }}</button>
             </div>
           </div>
         </div>
@@ -54,6 +54,21 @@
 export default {
   name: 'FormQuestion',
   mounted() {
+  },
+  data() {
+    return {
+      questionTitle: '',
+    }
+  },
+  computed: {
+    isValid() {
+      return this.questionTitle;
+    }
+  },
+  methods: {
+    newQuestion() {
+      alert(this.questionTitle);
+    }
   }
 };
 </script>
