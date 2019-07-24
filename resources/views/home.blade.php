9372045f05d6a7cfaf1ae269b970738d2a633f0e
Christian Torres <chtorrez@gmail.com>
2019-07-24 00:21:28 -0600

    Validating if there is anything to vote.

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,5 +1,13 @@
 @extends('layouts.app')
 
 @section('content')
+  @if ($question)  
   <survey-instance :question="{{ json_encode($question->toArray()) }}"></survey-instance>
+  @else
+  <div class="row">
+    <div class="col">
+      <h1 class="display-4 text-center">{{ __('There is nothing to vote, come back later.') }}</h1>
+    </div>
+  </div>
+  @endif
 @endsection
